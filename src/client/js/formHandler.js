8867925d07d8065362b3b25e19f763df2c86998f
Shadee Merhi <shadmerhi@gmail.com>
2020-08-26 20:33:14 -0600

    Updated API call to be much more efficient

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,6 +7,7 @@ function handleSubmit(event) {
     event.preventDefault()
 
     let formText = document.getElementById('text-entry').value;
+    document.getElementById('results').innerHTML = '';
     document.getElementById('user_input').innerHTML = formText;
 
     // POST request to server
@@ -22,38 +23,17 @@ function handleSubmit(event) {
         }
 
         const response = await fetch(url, options);
-
         try {
             const data = await response.json();
-            console.log(data);
+            document.getElementById('results').innerHTML = data.agreement;
         }catch(error) {
             console.log('error', error)
         }
     }
 
-    // console.log("User input posted to the server");
-
     postData('http://localhost:8081/userInput', {
         userInput: formText
     })
-
-    setTimeout(function () {
-        updateUI();
-    }, 4000);
-
-    const updateUI = async () => {
-        const request = await fetch('/all');
-        try {
-            const allData = await request.json();
-            console.log(allData);
-            document.getElementById('results').innerHTML = allData.agreement;
-        }catch(error){
-            console.log('error', error);
-        }
-        console.log('AFTER GET');
-    }
-
 }
-
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -30,17 +30,8 @@ app.listen(8081, function () {
 })
 
 
-// app.get('/test', function (request, response) {
-//     response.send(mockAPIResponse);
-// })
-
-
-
 // API work below
 
-// Empty JS object
-// let newInput = {};
-let projectData = {};
 let baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=';
 
 let apiKey = process.env.API_KEY;
@@ -48,51 +39,24 @@ let apiKey = process.env.API_KEY;
 // Posting user response from client to server
 app.post('/userInput', function (request, response) {
 
-    // console.log(request);
-
     let userInput = {
         input: request.body.userInput,
         lang: 'en'
     }
-    response.send('POST RECEIVED');
 
     console.log('User Input: ' + userInput.input);
-    console.log('API KEY: ' + apiKey);
     console.log(baseURL+apiKey+'&of=json&txt='+userInput.input+'.&lang=en');
 
-
     getTextAnalysis(baseURL, apiKey, userInput.input)
-    .then(function(data) {
-        postData('http://localhost:8081/addTextData', {
-            agreement: data.agreement
-        })
-        // console.log(data.agreement);
+    .then(function(data){
+        response.send(data);
     })
-
-    const postData = async (url = '', data = {}) => {
-        const response = await fetch(url, {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
-
-        try {
-            const newData = await response.json();
-            console.log('NEW DATA: ' + newData.agreement);
-        }catch(error) {
-            console.log('error', error);
-        }
-    }
 })
 
 
 const getTextAnalysis = async (baseURL, apiKey, input) => {
     console.log('FETCH URL: '+baseURL+apiKey+'&of=json&txt='+input+'.&lang=en');
 
-    // This fetch is currently where this are going wrong - invalid URL
     const response = await fetch(baseURL+apiKey+'&of=json&txt='+input+'.&lang=en');
     try {
         const newData = await response.json();
@@ -101,27 +65,4 @@ const getTextAnalysis = async (baseURL, apiKey, input) => {
         console.log('ERROR', error);
     }
 }
-
-// Posting API results
-app.post('/addTextData', function (request, response) {
-    let newEntry = {
-        agreement: request.body.agreement,
-    }
-    console.log('NEW ENTRY: ' + newEntry.agreement);
-    projectData = newEntry;
-    response.send(projectData);
-})
-
-app.get('/all', function (request, response) {
-    console.log('BEING SENT TO CLIENT ' + projectData);
-    response.send(projectData);
-    console.log('Response Sent');
-})
-
-
-
-
-// app.get('/api', function (request, response) {
-//     response.send({key: application_key});
-// })
 
